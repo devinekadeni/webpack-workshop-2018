7b96300d9f400ee387b8b6b6b30ea8fdde91f947
Devin <devinekadeni@gmail.com>
2019-05-26 20:17:40 +0700

    added image and webpack rules url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import { footer } from './footer'
 import makeButton from './button'
 import { makeColorStyle } from './button-styles'
 import buttonStyles from './button.css'
+import makeImage from './image'
+import imageUrl from './webpack-logo.jpg'
+
+const image = makeImage(imageUrl)
 
 const button = makeButton('Yay! A button!')
 button.style = makeColorStyle('cyan')
 document.body.appendChild(button)
+document.body.appendChild(image)
 document.body.appendChild(footer)
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,21 @@ module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
       output: {
         filename: 'bundle.js', // bundled file name
       },
+      module: {
+        rules: [
+          {
+            test: /\.jpe?g$/,
+            use: [
+              {
+                loader: 'url-loader', // managing the file on the disk, is file-loader jobs behind the scene
+                options: {
+                  limit: 5000, // if off limit, will create in disk directory and return as hashed url, otherwise it will be inline in base64 URI data file
+                },
+              },
+            ],
+          },
+        ],
+      },
       plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
     modeConfig(mode)
