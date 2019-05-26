fbd5f1c1093f180795949655befb036f6dacde90
Devin <devinekadeni@gmail.com>
2019-05-26 19:41:05 +0700

    added button style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import nav from './nav'
 import { footer } from './footer'
 import makeButton from './button'
 import { makeColorStyle } from './button-styles'
+import buttonStyles from './button.css'
 
 const button = makeButton('Yay! A button!')
 button.style = makeColorStyle('cyan')
